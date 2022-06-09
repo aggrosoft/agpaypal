7f25d11dfdc6dd787bc44a95f1c5be80a3ef9bf3
alexander <coding@aggrosoft.de>
2022-06-09 17:43:03 +0200

    chore: release v1.0.0-beta.0

--- metadata.php
+++ metadata.php
@@ -10,7 +10,7 @@ $aModule = array(
         'en' => 'PayPal Commerce Platform payment methods integration',
     ],
     'thumbnail'    => '',
-    'version'      => '0.0.0',
+    'version'      => '1.0.0-beta.0',
     'author'       => 'Aggrosoft GmbH',
     'controllers'  => [
         'aggrosoft_paypal_webhook' => \Aggrosoft\PayPal\Application\Controller\WebhookController::class,

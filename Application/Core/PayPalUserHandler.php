903a2a03679afaf3a477a2ba7b72d481453b1e42
alexander <coding@aggrosoft.de>
2022-07-14 10:53:17 +0200

    strip N/A from admin_area_1

--- Application/Core/PayPalUserHandler.php
+++ Application/Core/PayPalUserHandler.php
@@ -37,7 +37,8 @@ class PayPalUserHandler
         //@TODO: this really needs to be properly checked with test data
         $address = \VIISON\AddressSplitter\AddressSplitter::splitAddress($shipping->address->address_line_1);
 
-        $user->oxuser__oxcity = new \OxidEsales\Eshop\Core\Field($shipping->address->admin_area_1 . ($shipping->address->admin_area_1 ? ' ' : '') . $shipping->address->admin_area_2);
+        $adminArea1 = $shipping->address->admin_area_1 === 'N/A' ? '' : $shipping->address->admin_area_1;
+        $user->oxuser__oxcity = new \OxidEsales\Eshop\Core\Field($adminArea1 . ($adminArea1 ? ' ' : '') . $shipping->address->admin_area_2);
         $user->oxuser__oxzip = new \OxidEsales\Eshop\Core\Field($shipping->address->postal_code);
         $user->oxuser__oxstreet = new \OxidEsales\Eshop\Core\Field($address['additionToAddress1'] . $address['streetName']);
         $user->oxuser__oxstreetnr = new \OxidEsales\Eshop\Core\Field($address['houseNumber']);
